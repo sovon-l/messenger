c9ff129c99269b48ab01770de9aa03357a2ed5e0
orb li <mail@orbb.li>
2022-03-31 01:47:45 +0800

    complete pub sub

---
+++ src/publisher.rs
@@ -0,0 +1,36 @@
+
+pub trait Publisher<M> {
+    fn publish(&mut self, msg: M);
+}
+
+pub fn publisher_loop<M>(
+    rcv: crossbeam_channel::Receiver<M>,
+    mut publisher: impl Publisher<M>,
+){
+    loop {
+        match rcv.recv() {
+            Ok(msg) => publisher.publish(msg),
+            Err(e) => log::error!("{}", e),
+        }
+    }
+}
+
+// below are implementation examples
+
+// 1. zmq publisher
+pub type ZmqPublisher = zmq::Socket;
+
+impl<M: Into<zmq::Message> + Clone> Publisher<M> for ZmqPublisher {
+    fn publish(&mut self, msg: M) {
+        self.send(&msg, zmq::DONTWAIT).unwrap()
+    }
+}
+
+// 2.bus publisher
+pub type BusPublisher<M> = bus::Bus<M>;
+
+impl<M> Publisher<M> for BusPublisher<M> {
+    fn publish(&mut self, msg: M) {
+        self.broadcast(msg)
+    }
+}
--- src/subscriber.rs
+++ src/subscriber.rs
@@ -4,16 +4,81 @@ pub trait Listener<M> {
 }
 
 pub trait Subscriber<M> {
-    fn init(&mut self);
-    fn recv(&mut self) -> Result<M, Box<dyn std::error::Error>>;
+    fn init(&mut self) {}
+    fn recv(&mut self) -> Option<M>;
 }
 
-pub fn subscribe<M>(
+pub fn subscriber_loop<M>(
     mut subscriber: impl Subscriber<M>,
     mut listener: impl Listener<M>,
 ) {
     subscriber.init();
-    while let Ok(msg) = subscriber.recv() {
+    while let Some(msg) = subscriber.recv() {
         listener.on_msg(msg);
+    }
+    log::warn!("a subscription dropped!");
+}
+
+// below are implementation examples
+
+// 1. crossbeam subscriber
+pub type CrossbeamSubscriber<M> = crossbeam_channel::Receiver<M>;
+
+impl<M> Subscriber<M> for CrossbeamSubscriber<M> {
+    fn recv(&mut self) -> Option<M> {
+        match crossbeam_channel::Receiver::<M>::recv(self) {
+            Ok(m) => Some(m),
+            Err(e) => {
+                log::error!(
+                    "{}",
+                    serde_json::json!({"error":e.to_string(), "msg":"bus fail to recv msg"})
+                        .to_string()
+                );
+                None
+            }
+
+        }
+    }
+}
+
+// 2. bus subscriber
+pub type BusSubscriber<M> = bus::BusReader<M>;
+
+impl<M: Clone + Sync> Subscriber<M> for BusSubscriber<M> {
+    fn recv(&mut self) -> Option<M> {
+        match bus::BusReader::recv(self) {
+            Ok(m) => Some(m),
+            Err(e) => {
+                log::error!(
+                    "{}",
+                    serde_json::json!({"error":e.to_string(), "msg":"bus fail to recv msg"})
+                        .to_string()
+                );
+                None
+            }
+        }
+    }
+}
+
+// 3. zmq subscriber
+pub type ZmqSubscriber = zmq::Socket;
+
+// TODO: can implement for appropriate lifetime for better perf?
+impl<M: From<Vec<u8>>> Subscriber<M> for ZmqSubscriber {
+    fn init(&mut self) {
+        self.set_subscribe(b"").unwrap();
+    }
+    fn recv(&mut self) -> Option<M> {
+        match self.recv_bytes(0) {
+            Ok(m) => Some(M::from(m)),
+            Err(e) => {
+                log::error!(
+                    "{}",
+                    serde_json::json!({"error":e.to_string(), "msg":"zmq fail to recv byte"})
+                        .to_string()
+                );
+                None
+            }
+        }
     }
 }
