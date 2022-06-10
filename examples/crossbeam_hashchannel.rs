4d5b8b881dcb8ff5ab7969f39db97cb1e95d4bf2
orb li <mail@orbb.li>
2022-06-10 15:35:10 +0800

    fix example

--- examples/crossbeam_hashchannel.rs
+++ examples/crossbeam_hashchannel.rs
@@ -1,91 +1,100 @@
+static MSGS: [&'static str; 63] = [
+    "Lorem",
+    "ipsum",
+    "dolor",
+    "sit",
+    "amet",
+    "consectetur",
+    "adipiscing",
+    "elit",
+    "sed",
+    "doneiusmod",
+    "tempor",
+    "incididunt",
+    "ut",
+    "labore",
+    "et",
+    "dolore",
+    "magna",
+    "aliqua",
+    "Ut",
+    "enim",
+    "adnminim",
+    "veniam",
+    "quis",
+    "nostrud",
+    "exercitation",
+    "ullamco",
+    "laboris",
+    "nisi",
+    "utnaliquip",
+    "ex",
+    "ea",
+    "commodo",
+    "consequat",
+    "Duis",
+    "aute",
+    "irure",
+    "dolor",
+    "innreprehenderit",
+    "in",
+    "voluptate",
+    "velit",
+    "esse",
+    "cillum",
+    "dolore",
+    "eu",
+    "fugiat",
+    "nullanpariatur",
+    "Excepteur",
+    "sint",
+    "occaecat",
+    "cupidatat",
+    "non",
+    "proident",
+    "sunt",
+    "innculpa",
+    "qui",
+    "officia",
+    "deserunt",
+    "mollit",
+    "anim",
+    "id",
+    "est",
+    "laborum",
+];
+
 pub fn main() {
-    let (tx, rx) = messenger::hash_channel::new_hash_channel::<
+    let (mut tx, rx) = messenger::hash_channel::new_hash_channel::<
         String,
         messenger::externals::crossbeam::Sender<String>,
         messenger::externals::crossbeam::Receiver<String>,
         messenger::externals::crossbeam::CrossbeamChannels<String>,
     >(8);
 
-    let msgs: Vec<String> = vec![
-        " Lorem ",
-        " ipsum ",
-        " dolor ",
-        " sit ",
-        " amet, ",
-        " consectetur ",
-        " adipiscing ",
-        " elit, ",
-        " sed ",
-        " doneiusmod ",
-        " tempor ",
-        " incididunt ",
-        " ut ",
-        " labore ",
-        " et ",
-        " dolore ",
-        " magna ",
-        " aliqua. ",
-        " Ut ",
-        " enim ",
-        " adnminim ",
-        " veniam, ",
-        " quis ",
-        " nostrud ",
-        " exercitation ",
-        " ullamco ",
-        " laboris ",
-        " nisi ",
-        " utnaliquip ",
-        " ex ",
-        " ea ",
-        " commodo ",
-        " consequat. ",
-        " Duis ",
-        " aute ",
-        " irure ",
-        " dolor ",
-        " innreprehenderit ",
-        " in ",
-        " voluptate ",
-        " velit ",
-        " esse ",
-        " cillum ",
-        " dolore ",
-        " eu ",
-        " fugiat ",
-        " nullanpariatur. ",
-        " Excepteur ",
-        " sint ",
-        " occaecat ",
-        " cupidatat ",
-        " non ",
-        " proident, ",
-        " sunt ",
-        " innculpa ",
-        " qui ",
-        " officia ",
-        " deserunt ",
-        " mollit ",
-        " anim ",
-        " id ",
-        " est ",
-        " laborum ",
-    ].into_iter().map(|s| s.to_string()).collect();
+    let msgs: Vec<String> = MSGS.iter().map(|s| s.to_string()).collect();
 
     for msg in msgs.into_iter() {
         tx.send(msg).unwrap();
     }
 
-    let mut counter = 0;
+    let mut sizes = vec![];
     for rx in rx.receivers.into_iter() {
-        println!("counter {}", counter);
         let mut size = 0;
         while !rx.is_empty() {
             let _ = rx.recv().unwrap();
-            // println!("{}", msg);
             size += 1;
         }
-        println!("size {}", size);
-        counter += 1;
+        sizes.push(size);
     }
+    print!("container: ");
+    for (k, _) in sizes.iter().enumerate() {
+        print!("{:3} ", k);
+    }
+    println!();
+    print!("sizes:     ");
+    for s in sizes.iter() {
+        print!("{:3} ", s);
+    }
+    println!();
 }
